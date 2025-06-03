fbbe800b6b6115ae1d72acc13bb2a86aacadf085
pample <bonnier.vincentpro@gmail.com>
2025-06-03 11:11:08 +0200

    splashscreen tpl is ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashscreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/splashscreen/splashscreen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen',
+  imports: [],
+  templateUrl: './splashscreen.component.html',
+  styleUrl: './splashscreen.component.css'
+})
+export class SplashscreenComponent {
+
+}
