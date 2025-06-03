1cf7b6b1838abe11798aebeca622cb1ce85b9279
pample <bonnier.vincentpro@gmail.com>
2025-06-03 11:23:03 +0200

    splashsscreen double data binding

--- src/app/splashscreen/splashscreen.component.ts
+++ src/app/splashscreen/splashscreen.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-splashscreen',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './splashscreen.component.html',
   styleUrl: './splashscreen.component.css'
 })
 export class SplashscreenComponent {
-
+   username : string = "";
+   serverUrl : string = "";
+   
+   
 }
